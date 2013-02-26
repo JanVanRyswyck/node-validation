2fefbf5662795ef78aae26d968dd0c898fcb4ecf
Jan Van Ryswyck <jan.van.ryswyck@gmail.com>
2013-02-26 21:14:14 +0100

    Added some tests

--- test/string-not-empty-tests.js
+++ test/string-not-empty-tests.js
@@ -2,24 +2,72 @@ var should = require('chai').should(),
 	util = require('util'),
     Validator = require('../lib').Validator;
 
-describe('Given an object with a string property', function() {
-	var objectWithStringProperty, validationErrors;
+describe('Given a validator that verifies whether the string property of an object is not empty', function() {
+	var validator, validationErrors;
 
-	describe('When validating a valid string', function() {
+	before(function() {
+		validator = new StringNotEmptyValidator();
+	});
+
+	describe('When validating a valid string value', function() {
 		before(function() {
 			var objectWithStringProperty = {
 				stringProperty: 'this-is-a-valid-string'
 			};
 
-			var validator = new StringNotEmptyValidator();
 			validationErrors = validator.validate(objectWithStringProperty);
 		});
 
-		it('should not issue a validation error for a valid string', function() {
-			validationErrors.should.be.empty;
-		});	
+		it('Should not return any validation error', function() {
+			validationErrors.should.have.length(0);
+		});
 	});
 
+	describe('When validating an empty string value', function() {
+		before(function() {
+			var objectWithEmptyStringProperty = {
+				stringProperty: ''
+			};
+
+			validationErrors = validator.validate(objectWithEmptyStringProperty);
+		});
+
+		it('Should return a validation error that specifies the failing property', function() {
+			validationErrors.should.have.deep.property('[0].propertyName', 'stringProperty');
+		});
+
+		it('Should return a validation error that specifies a message which explains the error', function() {
+			validationErrors.should.have.deep.property('[0].message', 'stringProperty should not be empty.');
+		});
+	});
+
+	describe('When validating an undefined value', function() {
+		before(function() {
+			var objectWithUndefinedStringProperty = {
+				stringProperty: undefined
+			};
+
+			validationErrors = validator.validate(objectWithUndefinedStringProperty);
+		});
+
+		it('Should return a validation error', function() {
+			validationErrors.should.have.length(1);
+		});
+	});
+
+	describe('When validating a null value', function() {
+		before(function() {
+			var objectWithNullStringProperty = {
+				stringProperty: null
+			};
+
+			validationErrors = validator.validate(objectWithNullStringProperty);
+		});
+
+		it('Should return a validation error', function() {
+			validationErrors.should.have.length(1);
+		});
+	});
 });
 
 var StringNotEmptyValidator = function() {
